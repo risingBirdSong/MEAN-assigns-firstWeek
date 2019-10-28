ee3e38d8d3d44dd530d9f00b3a381ce166d4b94b
risingBirdSong <52929863+risingBirdSong@users.noreply.github.com>
2019-10-28 13:48:05 -0700

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,30 @@
+   let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+    students.forEach((student) => {
+      console.log(`name : ${student.name}, cohort : ${student.cohort}`);
+    })
+
+
+    let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+
+ Object.values(myusers).forEach((worker, outerIdx) => {
+  worker.forEach((w, innerIdx) => console.log(`${innerIdx} -> ${w.last_name} , ${w.first_name} lengthOfNames -> ${w.first_name.length + w.last_name.length}`));
+})
