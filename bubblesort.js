f4520603703b51353c795ed7f096e6e48bc96007
risingBirdSong <52929863+risingBirdSong@users.noreply.github.com>
2019-10-27 21:07:06 -0700

    Create bubblesort.js

---
+++ bubblesort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+    for (var i = 0; i <= arr.length; i++) {
+        for (var j = 0; j <= arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
