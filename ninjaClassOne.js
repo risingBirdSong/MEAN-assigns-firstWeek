39197f83e2bc9037b8b7b0791eb2a088d99afa76
risingBirdSong <52929863+risingBirdSong@users.noreply.github.com>
2019-10-29 11:34:49 -0700

    Update ninjaClassOne.js
    updated the file with punchh and kick

--- ninjaClassOne.js
+++ ninjaClassOne.js
@@ -1,6 +1,6 @@
 function Ninja(name, health = 100){
   var speed = 3;
-  var strength = 3;
+  var strength = 4;
   this.name = name;
   this.health = health;
 
@@ -28,13 +28,32 @@ Ninja.prototype.drinkSake = function (){
   console.log(`mmm sake, that was good, my health is ${this.health}`);
 }
 
+Ninja.prototype.punch = function(ninjaToBePunched){
+  ninjaToBePunched.health -= 5;
+  console.log(`${ninjaToBePunched.name} has been punched by ${this.name}, and their health is ${ninjaToBePunched.health}`);
+}
+
+Ninja.prototype.kick = function(ninjaToBeKicked){
+  console.log(this.sayStrength())
+  ninjaToBeKicked.health -= (this.sayStrength() * 15);
+  console.log(`${ninjaToBeKicked.name} has been kicked for ${this.sayStrength() * 15} and their health is ${ninjaToBeKicked.health}`);
+}
+
 // drinkSake() - This should add +10 Health to the Ninja
 //showStats() - This should show the Ninja's name, strength, speed, and health.
 
 let myNinja = new Ninja("greg");
+let punchingBag = new Ninja("horace");
+
 myNinja.sayName();
 myNinja.showStats();
 myNinja.drinkSake();
+
+myNinja.punch(punchingBag);
+myNinja.punch(punchingBag);
+myNinja.punch(punchingBag);
+myNinja.kick(punchingBag);
+
 
 
 
