c4e28cdfddf3d74a767170b860f4daf9c8e000a6
risingBirdSong <52929863+risingBirdSong@users.noreply.github.com>
2019-10-29 18:24:04 -0700

    Create landingpage.js

---
+++ landingpage.js
@@ -0,0 +1,26 @@
+'use strict';
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((request, response) => {
+
+    console.log(request.url);
+  if(request.url === '/') {
+    fs.readFile('index.html', 'utf8', (errors, contents) => {
+      response.writeHead(200, {'Content-Type': 'text/html',});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else if (request.url === '/ninjas') {
+    fs.readFile('ninjas.html', 'utf8', (errors, contents) => {
+      response.writeHead(200, {'Content-type': 'text/html',});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else {
+    response.end('File not found!!!');
+  }
+});
+server.listen(6789);
+console.log('listening on port 6789');
